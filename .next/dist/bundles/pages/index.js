89b912a9f53cc0e791fde96e3ba4dbaf9e6ff6e4
drewpete4 <drew.pete4@gmail.com>
2018-02-25 16:38:32 -0700

    first commit

---
+++ .next/dist/bundles/pages/index.js
@@ -0,0 +1,450 @@
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete installedModules[moduleId];
+/******/ 		}
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 15);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+module.exports = require("react");
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+module.exports = require("babel-runtime/regenerator");
+
+/***/ }),
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_material_ui_AppBar__ = __webpack_require__(3);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_material_ui_AppBar___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_material_ui_AppBar__);
+
+
+
+var Header = function Header(_ref) {
+  var _ref$title = _ref.title,
+      title = _ref$title === undefined ? "Next.js Blogging Application" : _ref$title;
+  return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_1_material_ui_AppBar___default.a, { title: title, showMenuIconButton: false });
+};
+
+/* harmony default export */ __webpack_exports__["a"] = (Header);
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+module.exports = require("material-ui/AppBar");
+
+/***/ }),
+/* 4 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+
+// EXTERNAL MODULE: external "babel-runtime/regenerator"
+var regenerator_ = __webpack_require__(1);
+var regenerator__default = /*#__PURE__*/__webpack_require__.n(regenerator_);
+
+// EXTERNAL MODULE: external "react"
+var external__react_ = __webpack_require__(0);
+var external__react__default = /*#__PURE__*/__webpack_require__.n(external__react_);
+
+// EXTERNAL MODULE: external "material-ui/styles/MuiThemeProvider"
+var MuiThemeProvider_ = __webpack_require__(5);
+var MuiThemeProvider__default = /*#__PURE__*/__webpack_require__.n(MuiThemeProvider_);
+
+// EXTERNAL MODULE: external "material-ui/styles/getMuiTheme"
+var getMuiTheme_ = __webpack_require__(6);
+var getMuiTheme__default = /*#__PURE__*/__webpack_require__.n(getMuiTheme_);
+
+// EXTERNAL MODULE: external "react-tap-event-plugin"
+var external__react_tap_event_plugin_ = __webpack_require__(7);
+var external__react_tap_event_plugin__default = /*#__PURE__*/__webpack_require__.n(external__react_tap_event_plugin_);
+
+// EXTERNAL MODULE: external "next/head"
+var head_ = __webpack_require__(8);
+var head__default = /*#__PURE__*/__webpack_require__.n(head_);
+
+// CONCATENATED MODULE: ./Shared/MUI/theme.js
+var PRIMARY_COLOR = "#569cb7";
+var PRIMARY_COLOR_TWO = "#88cde9";
+var PRIMARY_COLOR_THREE = "#1e6e87";
+var ACCENT_COLOR_ONE = "#556cb7";
+var ACCENT_COLOR_TWO = "#889aea";
+var ACCENT_COLOR_THREE = "#204287";
+// CONCATENATED MODULE: ./Shared/MUI/withMUI.js
+
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+
+
+
+
+
+
+
+try {
+  external__react_tap_event_plugin__default()();
+} catch (e) {
+  //can only be called once per application lifecycle
+}
+
+var withMUI_withMaterialUI = function withMaterialUI(ComposedComponent) {
+  var HOC = function (_Component) {
+    _inherits(HOC, _Component);
+
+    function HOC() {
+      _classCallCheck(this, HOC);
+
+      return _possibleConstructorReturn(this, (HOC.__proto__ || Object.getPrototypeOf(HOC)).apply(this, arguments));
+    }
+
+    _createClass(HOC, [{
+      key: "render",
+      value: function render() {
+        var userAgent = this.props.userAgent;
+
+        var Lato = "lato, sans-serif";
+        var muiTheme = getMuiTheme__default()({
+          fontFamily: Lato,
+          palette: {
+            primary1Color: PRIMARY_COLOR,
+            primary2Color: PRIMARY_COLOR_TWO,
+            primary3Color: PRIMARY_COLOR_THREE,
+            accent1Color: ACCENT_COLOR_ONE,
+            accent2Color: ACCENT_COLOR_TWO,
+            accent3Color: ACCENT_COLOR_THREE
+          },
+          appBar: {
+            height: 50
+          }
+        }, {
+          userAgent: userAgent
+        });
+        return external__react__default.a.createElement(
+          "div",
+          null,
+          external__react__default.a.createElement(
+            head__default.a,
+            null,
+            external__react__default.a.createElement(
+              "title",
+              null,
+              "Nextjs Blogger"
+            ),
+            external__react__default.a.createElement("meta", {
+              name: "viewport",
+              content: "initial-scale=1.0 width=device-width"
+            }),
+            external__react__default.a.createElement("link", {
+              href: "https://fonts.googleapis.com/css?family=Lato",
+              rel: "stylesheet"
+            })
+          ),
+          external__react__default.a.createElement(
+            MuiThemeProvider__default.a,
+            { muiTheme: muiTheme },
+            external__react__default.a.createElement(ComposedComponent, this.props)
+          )
+        );
+      }
+    }], [{
+      key: "getInitialProps",
+      value: function () {
+        var _ref = _asyncToGenerator( /*#__PURE__*/regenerator__default.a.mark(function _callee(ctx) {
+          var req, userAgent, subProps;
+          return regenerator__default.a.wrap(function _callee$(_context) {
+            while (1) {
+              switch (_context.prev = _context.next) {
+                case 0:
+                  req = ctx.req;
+                  userAgent = req ? req.headers["user-agent"] : navigator.userAgent;
+                  _context.next = 4;
+                  return ComposedComponent.getInitialProps(ctx);
+
+                case 4:
+                  subProps = _context.sent;
+                  return _context.abrupt("return", _extends({}, subProps, {
+                    userAgent: userAgent
+                  }));
+
+                case 6:
+                case "end":
+                  return _context.stop();
+              }
+            }
+          }, _callee, this);
+        }));
+
+        function getInitialProps(_x) {
+          return _ref.apply(this, arguments);
+        }
+
+        return getInitialProps;
+      }()
+    }]);
+
+    return HOC;
+  }(external__react_["Component"]);
+
+  return HOC;
+};
+
+/* harmony default export */ var withMUI = __webpack_exports__["a"] = (withMUI_withMaterialUI);
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports) {
+
+module.exports = require("material-ui/styles/MuiThemeProvider");
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports) {
+
+module.exports = require("material-ui/styles/getMuiTheme");
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports) {
+
+module.exports = require("react-tap-event-plugin");
+
+/***/ }),
+/* 8 */
+/***/ (function(module, exports) {
+
+module.exports = require("next/head");
+
+/***/ }),
+/* 9 */
+/***/ (function(module, exports) {
+
+module.exports = require("isomorphic-fetch");
+
+/***/ }),
+/* 10 */
+/***/ (function(module, exports) {
+
+module.exports = require("material-ui/Card");
+
+/***/ }),
+/* 11 */
+/***/ (function(module, exports) {
+
+module.exports = require("material-ui/RaisedButton");
+
+/***/ }),
+/* 12 */
+/***/ (function(module, exports) {
+
+module.exports = require("next/link");
+
+/***/ }),
+/* 13 */,
+/* 14 */,
+/* 15 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(16);
+
+
+/***/ }),
+/* 16 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_babel_runtime_regenerator__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_babel_runtime_regenerator___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_babel_runtime_regenerator__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_styled_jsx_style__ = __webpack_require__(17);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_styled_jsx_style___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_styled_jsx_style__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__components_header__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__Shared_MUI_withMUI__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_isomorphic_fetch__ = __webpack_require__(9);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_isomorphic_fetch___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_5_isomorphic_fetch__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6_material_ui_Card__ = __webpack_require__(10);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6_material_ui_Card___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_6_material_ui_Card__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7_material_ui_RaisedButton__ = __webpack_require__(11);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7_material_ui_RaisedButton___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_7_material_ui_RaisedButton__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8_next_link__ = __webpack_require__(12);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8_next_link___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_8_next_link__);
+
+
+var _this = this;
+
+
+
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+
+
+
+
+
+
+
+var Index = function Index(_ref) {
+  var posts = _ref.posts;
+  return __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(
+    "div",
+    {
+      className: "jsx-506946288"
+    },
+    __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_1_styled_jsx_style___default.a, {
+      styleId: "506946288",
+      css: [".post-link.jsx-506946288{text-decoration:none;color:#fff;font-size:18px;}"]
+    }),
+    __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_3__components_header__["a" /* default */], null),
+    posts.map(function (x) {
+      return __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(
+        __WEBPACK_IMPORTED_MODULE_6_material_ui_Card__["Card"],
+        { key: x.id },
+        __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_6_material_ui_Card__["CardHeader"], { title: x.title }),
+        __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(
+          __WEBPACK_IMPORTED_MODULE_6_material_ui_Card__["CardText"],
+          null,
+          __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(
+            __WEBPACK_IMPORTED_MODULE_7_material_ui_RaisedButton___default.a,
+            { fullWidth: true, primary: true },
+            __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(
+              __WEBPACK_IMPORTED_MODULE_8_next_link___default.a,
+              { href: "/post?id=" + x.id, as: "/blog/" + x.id },
+              __WEBPACK_IMPORTED_MODULE_2_react___default.a.createElement(
+                "a",
+                {
+                  className: "jsx-506946288" + " " + "post-link"
+                },
+                "Click to view post!"
+              )
+            )
+          )
+        )
+      );
+    })
+  );
+};
+
+Index.getInitialProps = _asyncToGenerator( /*#__PURE__*/__WEBPACK_IMPORTED_MODULE_0_babel_runtime_regenerator___default.a.mark(function _callee() {
+  var response, data;
+  return __WEBPACK_IMPORTED_MODULE_0_babel_runtime_regenerator___default.a.wrap(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return fetch("https://www.googleapis.com/blogger/v3/blogs/4789269094064278868/posts" + "?key=" + "AIzaSyDuO-f-UQ568WSPLnn8V4exKB6aeh-aZdI");
+
+        case 2:
+          response = _context.sent;
+          _context.next = 5;
+          return response.json();
+
+        case 5:
+          data = _context.sent;
+          return _context.abrupt("return", { posts: data.items });
+
+        case 7:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, _callee, _this);
+}));
+
+/* harmony default export */ __webpack_exports__["default"] = (Object(__WEBPACK_IMPORTED_MODULE_4__Shared_MUI_withMUI__["a" /* default */])(Index));
+
+/***/ }),
+/* 17 */
+/***/ (function(module, exports) {
+
+module.exports = require("styled-jsx/style");
+
+/***/ })
+/******/ ]);
---
+++ pages/about.js
@@ -0,0 +1,7 @@
+const About = ({ title = "Hello from the About Route" }) => (
+  <div>
+    <h2>{title}</h2>
+  </div>
+);
+
+export default About;
